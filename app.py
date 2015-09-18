d897f279fe04b5b362417deb6005fe16044a444d
swhh <seamus.holland1@gmail.com>
2015-09-18 19:22:48 +0100

    push first version

--- app.py
+++ app.py
@@ -10,11 +10,38 @@ from flask import Flask, render_template, request, send_file
 app = Flask(__name__)
 
 
+
+class MyLogger(object):
+
+    def __init__(self):
+        self.download_error = None
+        self.download_warning = None
+
+    def debug(self, msg):
+        pass
+
+    def warning(self, msg):
+        self.download_error = msg
+        print msg
+
+    def error(self, msg):
+        self.download_error = msg
+        print msg
+
+
 def video_id(url):
     """ return string of video id from url; null if non-existent """
     vid_id = re.search(r'\?v=(.+)', url)
     if vid_id:
         return vid_id.group(1)
+
+
+def my_hook(d):
+    status = d['status']
+    if status == 'downloading':
+        print 'Download started'
+    if status == 'finished':
+        print('Done downloading, now converting ...')
 
 
 def remove_files_in_directory(directory):
@@ -43,15 +70,27 @@ def valid_url(url):
 @app.route('/', methods=['GET', 'POST'])
 def hello():
     error = ""
+    remove_files_in_directory('videos')  # remove any already downloaded videos
     if request.method == 'POST':
-        remove_files_in_directory('videos')  # remove any already downloaded videos
         url = request.form['url']
+
         if valid_url(url):
-            ydl_options = {'outtmpl': 'videos/%(id)s.%(ext)s'}
-            with youtube_dl.YoutubeDL(ydl_options) as ydl:
-                ydl.download([url])
+            my_logger = MyLogger()
+            ydl_options = {
+                'outtmpl': 'videos/%(id)s.%(ext)s',
+                'logger': my_logger,
+                'progress_hooks': [my_hook]
+            }
+
+            try:
+                with youtube_dl.YoutubeDL(ydl_options) as ydl:
+                    ydl.download([url])
+            except:
+                return render_template('index.html', error=my_logger.download_error)
+
             file_name = glob.glob('videos/' + video_id(url) + '*')[0]  # find file in directory containing video id
             return send_file(file_name, as_attachment=True)
+
         error = "Please enter a valid YouTube url"
 
     return render_template('index.html', error=error)
